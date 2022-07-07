cf345df4a1f240b2fa94ed775fe111f94f0f759b
Li Wang <liwang@Wenchengs-MacBook-Pro.local>
2022-07-07 12:12:16 -0600

    Li's 5th commit

--- RenderCode.R
+++
@@ -1,5 +0,0 @@
-rmarkdown::render("Project2.Rmd", 
-                  output_format = "github_document",
-                  output_file = "README.md",
-                  output_options = list(html_preview= FALSE,toc=TRUE,toc_depth=2,toc_float=TRUE)
-)
